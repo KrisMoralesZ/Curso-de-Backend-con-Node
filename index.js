d5b699878828b81023db27678e27792e23ddeff4
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2022-11-29 17:29:58 -0700

    category and products

--- index.js
+++ index.js
@@ -6,6 +6,36 @@ app.get('/', (req, res) => {
   res.send('Hello this is my express server');
 })
 
+app.get('/new-route', (req, res) => {
+  res.json([
+    {
+      name: 'Product 1',
+      price: 1000,
+    },
+    {
+      name: 'Product 2',
+      price: 1500,
+    }]);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params
+  res.json(
+    {
+      id,
+      name: 'Product 2',
+      price: 2000
+    }
+  );
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params
+  res.json({
+    categoryId,
+    productId,
+  })
+});
+
 app.listen(port, () => {
-  console.log('Mi primer server');
 })

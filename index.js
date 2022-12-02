8bbfe2b514c3b535da65ad8613d8379af24a24f2
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2022-12-01 17:41:16 -0700

    single responsability

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const faker = require('faker')
 
 const app = express();
 const port = 3000;
@@ -11,37 +10,6 @@ app.get('/', (req, res) => {
 app.get('/new-route', (req, res) => {
   res.send("Hello I'm a new route");
 });
-
-app.get('/products', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let i = 0; i < limit; i++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
-  res.json(products);
-});
-
-app.get('/products/filter', (req, res) => {
-  res.send('hello');
-})
-
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params
-  res.json(
-    {
-      id,
-      name: 'Product 2',
-      price: 2000
-    }
-  );
-});
-
-
 
 app.get('/categories/:categoryId/products/:productId', (req, res) => {
   const { categoryId, productId } = req.params

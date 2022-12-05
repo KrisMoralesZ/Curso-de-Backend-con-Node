2507188443309437aef0f17d74cc1af620c075df
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2022-12-05 09:33:27 -0700

    api v1

--- index.js
+++ index.js
@@ -1,35 +1,17 @@
 const express = require('express');
+const routerApi = require('./routes');
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello this is my express server');
-})
-
-app.get('/new-route', (req, res) => {
-  res.send("Hello I'm a new route");
 });
 
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params
-  res.json({
-    categoryId,
-    productId,
-  })
-});
-
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset
-    });
-  } else {
-    res.send('There is no params')
-  }
-})
+routerApi(app);
 
 app.listen(port, () => {
+  console.log('Mi port' + port)
 })
